89966f3719f958d458fb5050999e0b74323ffed2
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2022-04-21 01:19:34 +0800

    fix: change name to PanicException from PanicError

--- ocrdgen/exception/exception.py
+++ ocrdgen/exception/exception.py
@@ -1,4 +1,5 @@
-
+# taken from https://github.com/oh-my-ocr/text_renderer/blob/8fe3260ec952128d4d1b18dd41a7a82efa8f50c4/text_renderer/utils/errors.py
+# with small modification
 
 class PanicException(Exception):
     """If this error raised, render process should stop because some non-random error has occurred"""
